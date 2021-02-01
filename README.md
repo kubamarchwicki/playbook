b8f9252d41dbdc4087d29ca9a7ad7ee9f26b4724
kubamarchwicki <kuba@marchwicki.pl>
2021-02-01 09:55:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+My Ansible-backed Linux personal configuration.
+
+Run with:
+
+```bash -c "$(curl -fsSL https://raw.githubusercontent.com/kubamarchwicki/playbook/master/start.sh)"```
---
+++ start.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+set -e
+
+REPOSITORY="git@github.com:kubamarchwicki/playbook.git"
+TARGET="$(pwd)"
+
+RED=""
+GREEN=""
+BLUE=""
+RESET=""
+
+error() {
+    echo "${RED}""Error: $*""${RESET}" >&2
+    exit 1
+}
+
+ok() {
+    echo "${GREEN}""Info   | OK        | $*""${RESET}"
+}
+
+installing() {
+    echo "${BLUE}""Info   | Install   | $*""${RESET}"
+}
+
+generate_temp_dir() {
+    PLAYBOOK_LOCATION=$(mktemp -d -t playbookXXX)
+    trap 'rm -rf "$PLAYBOOK_LOCATION"' EXIT
+    git clone -q --depth=1 "${REPOSITORY}" "$PLAYBOOK_LOCATION" || error "git clone of playbook repo failed, run with --local if already cloned"
+    TARGET="$PLAYBOOK_LOCATION"
+}
+
+if [ -t 1 ]; then
+    RED=$(printf '\033[31m')
+    GREEN=$(printf '\033[32m')
+    BLUE=$(printf '\033[34m')
+    RESET=$(printf '\033[m')
+fi
+
+if ! command -v git &> /dev/null;
+    then
+        installing "git" && sudo apt update && sudo apt python-apt && sudo apt install git
+    else
+        ok "git"
+fi
+
+[[ "$1" = "--local" ]] && echo "Using local copy" || generate_temp_dir
+
+for arg do
+  shift
+  [ "$arg" = "--local" ] && continue
+  set -- "$@" "$arg"
+done
+
+
+if ! command -v ansible &> /dev/null;
+    then
+        installing "ansible" && apt update && apt install ansible
+    else
+        ok "ansible"
+fi
+
+cd "$TARGET" && ansible-playbook playbook.yml -K "$@"
